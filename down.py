c08d6f31182f2d7a8b88955fee348b39f5df0e1d
akshat9000 <40146469+akshat9000@users.noreply.github.com>
2020-03-29 15:26:45 +0530

    Playlist Downloader specefic to vidmo.org
    I will be working to make it generic for all sites

--- down.py
+++ down.py
@@ -1,34 +1,45 @@
-import os
 from selenium import webdriver
+import time
 from bs4 import BeautifulSoup
-import requests
-import html5lib
 from selenium.webdriver.chrome.options import Options
+import os
+import os.path
 
-# chrome_options = Options()
-# chrome_options.add_argument("--headless")
+opts = Options()
+opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
+browser = webdriver.Chrome(executable_path='C:\\Users\\Akshat Srivastava\\Downloads\\chromedriver',options=opts)
 
-# browser = webdriver.Chrome(executable_path='C:\\Users\\Akshat Srivastava\\Downloads\\chromedriver',options=chrome_options)
-browser = webdriver.Chrome(executable_path='C:\\Users\\Akshat Srivastava\\Downloads\\chromedriver')
-
-url1 = 'https://www.youtube.com/watch?v=bytnxnZFLeg'
-
-def getBlob(url):
-	browser.get(url)
-	soup = BeautifulSoup(browser.page_source,'html.parser')
-	blob = soup.find('video',{"src":True})
-	return blob['src']
+dpath = 'C:\\Users\\Akshat Srivastava\\Downloads\\'
 
 def getURLlist():
-	lines = open('urls.txt','r').read().splitlines()
-	return lines
+    lines = open('urls.txt','r').read().splitlines()
+    return lines
 
 def blobsAll():
-	lis = getURLlist()
-	bl = []
-	for i in lis:
-		bl.append(getBlob(i))
-	print(bl)
-	browser.quit()
+    lis = getURLlist()
+    bl = []
+    for i in lis:
+        bl.append(i)
+    return bl
 
-blobsAll()
+def getLink(url):
+	browser.get(url)
+	return browser.find_element_by_xpath('/html/body/div[1]/div[2]/div[6]/div/video').get_attribute('src')
+
+def createName(st2):
+	st2 = st2.split('/')[-1].split('.')[0][10:]
+	st = dpath + st2 + '.mp4'
+	return st2, st
+
+def down():
+	ls = blobsAll()
+	for url in ls:
+		du = getLink(url)
+		st2, st = createName(du)
+		browser.get(du)
+		while(not os.path.isfile(st)):
+			print('downloading '+st2+' . . .')
+			time.sleep(30)
+
+down()
+browser.quit()
